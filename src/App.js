86054b103e222421bcbac603ff112433c3d27d3d
huumanh1612 <21522331@gm.uit.edu.vn>
2024-09-22 18:33:11 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Post from "./pages/Post";
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
+import { AuthContext } from "./helpers/AuthContext";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [authState, setAuthState] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/auth/auth", {
+        headers: { accessToken: localStorage.getItem("accessToken") },
+      })
+      .then((respond) => {
+        if (respond.data.error) setAuthState(false);
+        else setAuthState(true);
+      });
+    setAuthState(true);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthContext.Provider value={{ authState, setAuthState }}>
+        <Router>
+          <div className="navbar">
+            <Link to="/">Home</Link>
+            <Link to="/createpost">Create A Post</Link>
+            {!authState && (
+              <>
+                <Link to="/login">Login</Link>
+                <Link to="/registration">Registration</Link>
+              </>
+            )}
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/createpost" element={<CreatePost />} />
+            <Route path="/post/:id" element={<Post />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </AuthContext.Provider>
     </div>
   );
 }
